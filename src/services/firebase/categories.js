4ad631c63740f0b1acee8008dc8a270615602cd9
Acid-Blast <marcelo.fa.lasca@hotmail.com>
2023-04-22 19:18:45 -0300

    categories order navbar

--- src/services/firebase/categories.js
+++ src/services/firebase/categories.js
@@ -1,8 +1,8 @@
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, query, orderBy } from 'firebase/firestore';
 import { db } from '../../services/firebase/firebaseConfig';
 
 export const getCategories = () => {
-    const categoriesRef = collection(db, 'categories')
+    const categoriesRef = query(collection(db, 'categories'), orderBy('label', 'asc'))
     return getDocs(categoriesRef)
         .then(snapshot => {
             return snapshot
