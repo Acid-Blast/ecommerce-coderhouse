f4d4ceaf43b843e0c016b18b6aed2cf3e54e4ca3
Acid-Blast <marcelo.fa.lasca@hotmail.com>
2023-04-27 00:56:27 -0300

    orderList highlight text-shadow

--- src/components/OrderDetails/OrderDetails.js
+++ src/components/OrderDetails/OrderDetails.js
@@ -22,16 +22,16 @@ const OrderDetails = () => {
 
     return (
         <div className="order-container">
-            <h1 className="bold">Detalles de la orden</h1>
+            <h1>Detalles de la orden</h1>
             <div className="order__data">
-                <h2><span className="bold">Codigo de orden: </span>{orderId}</h2>
-                <h3><span className="bold">Fecha: </span>{newOrder.date}</h3>
-                <h3><span className="bold">Nombre: </span>{newOrder.name} {newOrder.lastname}</h3>
-                <h3><span className="bold">Mail: </span>{newOrder.email}</h3>
-                <h3><span className="bold">Telefono: </span>{newOrder.phone}</h3>
-                <h3><span className="bold">Dirección de entrega: </span>{newOrder.address}</h3>
+                <h2><span className="whiteshadow">Codigo de orden: </span>{orderId}</h2>
+                <h3><span className="whiteshadow">Fecha: </span>{newOrder.date}</h3>
+                <h3><span className="whiteshadow">Nombre: </span>{newOrder.name} {newOrder.lastname}</h3>
+                <h3><span className="whiteshadow">Mail: </span>{newOrder.email}</h3>
+                <h3><span className="whiteshadow">Telefono: </span>{newOrder.phone}</h3>
+                <h3><span className="whiteshadow">Dirección de entrega: </span>{newOrder.address}</h3>
                 {
-                    newOrder.comments !== "" && <h3><span className="bold">Comentarios: </span>{newOrder.comments}</h3>
+                    newOrder.comments !== "" && <h3><span className="whiteshadow">Comentarios: </span>{newOrder.comments}</h3>
                 }
             </div>
 
@@ -48,13 +48,14 @@ const OrderDetails = () => {
                 </li>
             ))
             }
-                <li><span className="separador"> </span></li>
                 <li className="order__list__total"><h3>Total: ${newOrder.total}</h3></li>
             </ul>
 
             <p>
                 El pedido ya está listo! Pronto recibiras un correo con los detalles de la orden y el envio.
             </p>
+
+                <img alt="logo" src="https://firebasestorage.googleapis.com/v0/b/backend-prod-sublimart.appspot.com/o/logo.png?alt=media&token=8d281d3f-3244-4ab5-8c7d-8e905b4e9820"></img>
         </div>
     )
 }
